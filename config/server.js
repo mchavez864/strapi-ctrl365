5056f540336b018bc1c721db3bcd916a1989aa9d
mchavez864 <mchavez@ctrl365.com>
2025-11-18 20:47:30 -0300

    Update server.js

--- config/server.js
+++ config/server.js
@@ -3,15 +3,14 @@ module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
   app: {
-    // APP_KEYS debe venir como CSV en env y Strapi espera array
-    keys: env.array('APP_KEYS', ['defaultKey1', 'defaultKey2']),
+    keys: env.array('APP_KEYS'),
+  },
+  webhooks: {
+    populateRelations: env.bool('WEBHOOKS_POPULATE_RELATIONS', false),
   },
   admin: {
     auth: {
       secret: env('ADMIN_JWT_SECRET'),
     },
-  },
-  webhooks: {
-    populateRelations: env.bool('WEBHOOKS_POPULATE_RELATIONS', false),
   },
 });
