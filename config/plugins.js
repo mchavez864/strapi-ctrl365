06c1190136477a6ab818931182ec839f268a2629
mchavez864 <mchavez@ctrl365.com>
2025-11-19 02:24:16 -0300

    Update plugins.js

--- config/plugins.js
+++ config/plugins.js
@@ -1,17 +1,18 @@
-// ./config/plugins.js
+// config/plugins.js
 module.exports = ({ env }) => ({
   upload: {
     config: {
-      provider: 'strapi-provider-upload-azure-sa',
+      provider: "strapi-provider-upload-azure-sa",
       providerOptions: {
-        account: env('AZURE_ACCOUNT_NAME'),
-        accountKey: env('AZURE_ACCOUNT_KEY', ''),
-        containerName: env('AZURE_CONTAINER_NAME'),
-        defaultPath: env('AZURE_DEFAULT_PATH', 'uploads'),
-        defaultCacheControl: env('AZURE_DEFAULT_CACHE_CONTROL', 'public, max-age=31536000, immutable'),
-        removeContainerNameInUrl: env.bool('AZURE_REMOVE_CN', true),
+        account: env("AZURE_ACCOUNT_NAME"),
+        accountKey: env("AZURE_ACCOUNT_KEY", ""),
+        containerName: env("AZURE_CONTAINER_NAME"),
+        defaultPath: env("AZURE_DEFAULT_PATH", "uploads"),
+        defaultCacheControl: env("AZURE_DEFAULT_CACHE_CONTROL", "public, max-age=31536000, immutable"),
+        removeContainerNameInUrl: env.bool("AZURE_REMOVE_CN", true),
       },
     },
   },
 });
+
 
