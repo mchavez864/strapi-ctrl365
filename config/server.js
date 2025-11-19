4e921d2d2598ed5136c560d31f0c71e8946096eb
mchavez864 <mchavez@ctrl365.com>
2025-11-18 22:37:52 -0300

    Update server.js

--- config/server.js
+++ config/server.js
@@ -1,9 +1,9 @@
-// ./config/server.js
+// config/server.js
 module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
   app: {
-    keys: env.array('APP_KEYS', ['defaultKey1', 'defaultKey2']),
+    keys: env.array('APP_KEYS'),
   },
   webhooks: {
     populateRelations: env.bool('WEBHOOKS_POPULATE_RELATIONS', false),
