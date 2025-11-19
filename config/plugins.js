ab8b776229cdc00d276a5022fac65fe08296b076
mchavez864 <mchavez@ctrl365.com>
2025-11-18 22:37:25 -0300

    Update plugins.js

--- config/plugins.js
+++ config/plugins.js
@@ -1,4 +1,4 @@
-// ./config/plugins.js
+// config/plugins.js
 module.exports = ({ env }) => ({
   upload: {
     config: {
@@ -8,12 +8,13 @@ module.exports = ({ env }) => ({
         accountKey: env('AZURE_ACCOUNT_KEY'),
         containerName: env('AZURE_CONTAINER_NAME'),
         defaultPath: env('AZURE_DEFAULT_PATH', 'uploads'),
-        azureUploadSasToken: env('AZURE_SAS_TOKEN', null), // opcional
+        azureUploadSasToken: env('AZURE_SAS_TOKEN', null),
         removeContainerName: env('AZURE_REMOVE_CN') === 'true',
         maxConcurrent: 10,
-        serviceBaseURL: env('AZURE_SERVICE_BASE_URL', null), // opcional CDN
+        serviceBaseURL: env('AZURE_SERVICE_BASE_URL', null),
       },
     },
   },
 });
+
 
