7e92891816862207aaa5a1f0bfdb1f7c564c2c92
mchavez864 <mchavez@ctrl365.com>
2025-11-18 23:45:59 -0300

    Update server.js

--- config/server.js
+++ config/server.js
@@ -1,11 +1,23 @@
 // ./config/server.js
+
 module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
   app: {
-    keys: env.array('APP_KEYS'),
+    // Usamos el secreto STRAPI_APP_KEYS como arreglo
+    keys: env.array('STRAPI_APP_KEYS'),
   },
   webhooks: {
     populateRelations: env.bool('WEBHOOKS_POPULATE_RELATIONS', false),
   },
+  admin: {
+    auth: {
+      secret: env('ADMIN_STRAPI_JWT_SECRET'),
+    },
+  },
+  // JWT para API
+  jwt: {
+    secret: env('STRAPI_JWT_SECRET'),
+  },
 });
+
