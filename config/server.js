fe7aba635dbc4885c74c18da1eb5db48ea119ced
mchavez864 <mchavez@ctrl365.com>
2025-11-19 11:41:27 -0300

    Update server.js

--- config/server.js
+++ config/server.js
@@ -3,7 +3,7 @@ module.exports = ({ env }) => ({
   host: env("HOST", "0.0.0.0"),
   port: env.int("PORT", 1337),
   app: {
-    // Usamos APP_KEYS, que es lo que tenés en .env.example y en los secrets
+    // Usamos APP_KEYS, igual que en .env.example y en el set-env-vars del workflow
     keys: env.array("APP_KEYS"),
   },
 });
