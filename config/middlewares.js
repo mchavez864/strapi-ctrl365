b6dd3f898174e9a0def8ce10b028019377dbc4e8
mchavez864 <mchavez@ctrl365.com>
2025-11-19 18:12:07 -0300

    Update middlewares.js

--- config/middlewares.js
+++ config/middlewares.js
@@ -1,7 +1,34 @@
 module.exports = [
   'strapi::logger',
   'strapi::errors',
-  'strapi::security',
+  {
+    name: 'strapi::security',
+    config: {
+      contentSecurityPolicy: {
+        useDefaults: true,
+        directives: {
+          'connect-src': ["'self'", 'https:'],
+          'img-src': [
+            "'self'",
+            'data:',
+            'blob:',
+            'dl.airtable.com',
+            'https://market-assets.strapi.io',
+            // 👇 usamos la storage account real desde AZURE_ACCOUNT_NAME
+            `https://${process.env.AZURE_ACCOUNT_NAME}.blob.core.windows.net`,
+          ],
+          'media-src': [
+            "'self'",
+            'data:',
+            'blob:',
+            'dl.airtable.com',
+            `https://${process.env.AZURE_ACCOUNT_NAME}.blob.core.windows.net`,
+          ],
+          upgradeInsecureRequests: null,
+        },
+      },
+    },
+  },
   'strapi::cors',
   'strapi::poweredBy',
   'strapi::query',
@@ -10,3 +37,4 @@ module.exports = [
   'strapi::favicon',
   'strapi::public',
 ];
+
